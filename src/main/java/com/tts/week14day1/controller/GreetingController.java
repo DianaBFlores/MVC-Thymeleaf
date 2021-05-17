34d4c18a4b32759ff67b3af2e255fcc339e5a96e
DianaBFlores <64798817+DianaBFlores@users.noreply.github.com>
2021-05-17 14:53:00 -0400

    class notes

---
+++ src/main/java/com/tts/week14day1/controller/GreetingController.java
@@ -0,0 +1,55 @@
+package com.tts.week14day1.controller;
+
+import com.tts.week14day1.model.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+//add @Controller annotation
+@Controller
+public class GreetingController {
+    // @Controller is an extension of @Component
+// this means spring will pick it up and utilize the component
+// @Controller does not automatically have every method's return
+// act as a response body -- this is only true for @RestController
+
+    @GetMapping("/hello")
+    @ResponseBody // this annotation tells spring to use the return as a response
+    public String helloWorld() {
+        return "hello world!";
+    }
+
+    // since this method is used to resolve a template
+    // we do not say @ResponseBody
+    // take note that @GetMapping is the same thing as saying
+    // @RequestMapping(value ="/getting", method = RequestMethod.Get)
+
+    //add @GetMapping annotation for /greeting
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/greeting/var/{name}")
+    @ResponseBody
+    public String greetingWithPathVar(@PathVariable String name){
+        return "hello there, " + name;
+    }
+
+    // the request param here is not considered part of your  endpoint
+    // think of it more as an extension of your endpoint
+    @GetMapping("/greeting/param/")
+    @ResponseBody
+    public String greetingWithParam(@RequestParam(name="name",
+            required = false,
+            defaultValue="World") String name) {
+        return "hello there, " + name;
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
+}
